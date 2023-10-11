67884e1e0374672304d033e79c2bb0f917a58a85
ThiagoThalisson <atomic-feast@proton.me>
2023-10-11 06:49:32 -0300

    Added some basic theming colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        accent: {
+          light: "#33EEFF",
+          base: "#00BBCC",
+          dark: "#24868F",
+          darkest: "#1C464A",
+        },
+        primary: {
+          light: "#6666FF",
+          base: "#5252E0",
+          dark: "#4040BF",
+          darkest: "#1C1C4A",
+        },
+      },
+    },
+  },
+  plugins: [],
 }
