a693737daeb098da3961756f20718a909875c0af
ThiagoThalisson <atomic-feast@proton.me>
2023-10-23 08:40:21 -0300

    Added custom colors fixed some styles in global and created button component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -15,6 +16,22 @@ module.exports = {
           base: "#5252E0",
           dark: "#4040BF",
           darkest: "#1C1C4A",
+        },
+        dark: {
+          title: "#F1F5F9",
+          paragraph: "#CBD5E1",
+          fill: "#0F172A",
+          foregroundFill: "#1E293B",
+          navbarFill: "#334155",
+          footerFill: "#334155",
+        },
+        light: {
+          title: "#1E293B",
+          paragraph: "#475569",
+          fill: "#E2E8F0",
+          foregroundFill: "#F1F5F9",
+          navbarFill: "#F8FAFC",
+          footerFill: "#F8FAFC",
         },
       },
     },
