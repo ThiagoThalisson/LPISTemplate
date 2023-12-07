8fbf2ace230b258552117307fcd116d68b952184
ThiagoThalisson <atomic-feast@proton.me>
2023-12-07 15:25:57 -0300

    New Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,11 @@ import { defineConfig } from "astro/config"
 
 import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
-import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     sitemap({
       changefreq: "weekly",
       priority: 0.7,
